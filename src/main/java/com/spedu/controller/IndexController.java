9c28f8e3a6a6184cf6eca3ea83b68b9b48d3ac12
spedu <sp2264522742@outlook.com>
2023-03-09 20:15:57 +0800

    third commit

--- src/main/java/com/spedu/controller/IndexController.java
+++ src/main/java/com/spedu/controller/IndexController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping("/")
     public String index(){
-        return "你好git,哈哈哈";
+        return "你好git,hhhhhhhh";
     }
 }
