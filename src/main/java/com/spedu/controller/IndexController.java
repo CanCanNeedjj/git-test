725bd627d572eb2152f860725c5da2b38dd2887b
spedu <sp2264522742@outlook.com>
2023-03-09 20:11:47 +0800

    first commit

---
+++ src/main/java/com/spedu/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.spedu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Create by sanqiu
+ * Data 2023/3/9 20:07
+ */
+@RestController
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "你好git";
+    }
+}
