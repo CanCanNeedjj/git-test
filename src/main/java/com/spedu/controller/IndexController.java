02f91485081ee6015bd0a910ec727b69aa8065ea
spedu <sp2264522742@outlook.com>
2023-03-09 20:25:05 +0800

    hot-fix commit2

--- src/main/java/com/spedu/controller/IndexController.java
+++ src/main/java/com/spedu/controller/IndexController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping("/")
     public String index(){
+        System.out.println("fixfixfix");
         return "你好git,hhhhhhhh,fix";
     }
 }
